49100b46bc90945536a1b2991aced14ee97ac93a
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2026-04-01 14:31:23 -0600

    updates to set_up_pipeline.R

--- R/set_up_pipeline.R
+++ R/set_up_pipeline.R
@@ -16,11 +16,13 @@
 #' scc_library <- file.path(Sys.getenv("USERPROFILE"), 
 #'                          "USDA/Mountain Planning Service Group - SCC Library", 
 #'                          "12_Lolo NF")
-#' set_up_pipeline(file.path(scc_library, "Species List"), "LNF")
+#' spp_lst_dir <- file.path(scc_library, "Species List")
+#' set_up_pipeline(dir_path = spp_lst_dir, unit_code = "LNF")
 #' }
 set_up_pipeline <- function(dir_path, unit_code){
   template_dir = system.file("pipeline", package = "psoSppEvals")
+  if(!dir.exists(dir_path)) dir.create(dir_path, recursive = TRUE)
+  fs::dir_copy(template_dir, dir_path)
   pipeline_dir = file.path(dir_path, paste0(unit_code, "_SppEval_Pipeline"))
-  if(!dir.exists(pipeline_dir)) dir.create(pipeline_dir, recursive = TRUE)
-  fs::dir_copy(template_dir, pipeline_dir)
+  file.rename(file.path(dir_path, "pipeline"), pipeline_dir)
   }
